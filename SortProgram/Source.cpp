2980a8a10b3f15b30369894f0b279c986d3f868f
Adam Kelly <adam.mark.kelly@gmail.com>
2018-03-29 23:55:47 +0100

    Dev2/Rev.4 - Added comments

--- SortProgram/Source.cpp
+++ SortProgram/Source.cpp
@@ -12,18 +12,18 @@ void writeout();
 
 void readin()
 {
-	ifstream thefile1("numbers.txt");
-	thefile1.unsetf(std::ios_base::skipws);
+	ifstream thefile1("numbers.txt");//Textfile declaration
+	thefile1.unsetf(std::ios_base::skipws);//Skip new lines
 	// count the newlines with an algorithm specialized for counting:
 	unsigned line_count = std::count(
 		std::istream_iterator<char>(thefile1),
 		std::istream_iterator<char>(),
 		'\n');
-	cout << line_count << '\n';
+	cout << line_count << '\n';//Print out the number of lines starting from 0
 
 	int* a = NULL;   // Pointer to int, initialize to nothing.
 	int n;           // Size needed for array
-	n = line_count;        // Read in the size
+	n = line_count;        // allocate the size of the array
 	a = new int[n];  // Allocate n ints and save ptr in a.
 	for (int i = 0; i<n; i++) {
 		a[i] = 0;    // Initialize all elements to zero.
@@ -31,20 +31,21 @@ void readin()
 
 
 
-	string line;
-	ifstream thefile("numbers.txt");
-	if (thefile.is_open())
+	string line;//declare line 
+	ifstream thefile("numbers.txt");//Declare the txt file to read in from
+	if (thefile.is_open())//If the file is open do
 	{
-		int i = 0;
+		int i = 0;//Counter set to zero
 		while (getline(thefile, line))
 		{
-			a[i] = atoi(line.c_str());
-			cout << line << '\n';
-			i++;
+			a[i] = atoi(line.c_str());//put the conversion of line into --> a[i]
+			cout << line << '\n';//Print out the line as a check
+			i++;//increment the counter
 		}
-		thefile.close();
+		thefile.close();//Close the file
 	}
-	_getch();
+	_getch();//wait
+	//TODO: For making this into a full read in function, it should return the array so the sort can process the data
 }
 void writeout()
 {
