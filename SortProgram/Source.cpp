e4ecf54da57f1f0ebab10565a9c701748ded36c0
Adam Kelly <adam.mark.kelly@gmail.com>
2018-03-31 22:51:10 +0100

    BSD/Rev.1 - BubbleSort Inclusions (without file read in)

--- SortProgram/Source.cpp
+++ SortProgram/Source.cpp
@@ -1,4 +1,3 @@
-//File operation
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
@@ -8,8 +7,63 @@
 using namespace std;
 
 void readin();
-void writeout();
+void writeoutfile();
+//BubbleSort
+class bubbleSort
+{
+public:
+	void bubbleSortDriver();
 
+private:
+	void swap(int *xp, int *yp);//This swaps ints around for the sort
+	void sort(int arr[], int n);//This sorts the ints
+	void printArray(int arr[], int size);//This prints the sorted array out
+};
+
+void bubbleSort::bubbleSortDriver()//Driver
+{
+	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };//This needs to be changed to the array that has been loaded from the textfile
+	int n = sizeof(arr) / sizeof(arr[0]);
+	sort(arr, n);
+	printf("Sorted array: \n");
+	printArray(arr, n);
+	_getch();
+}
+void bubbleSort::swap(int *xp, int *yp)//Swaps ints around for the sort
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void bubbleSort::sort(int arr[], int n)// An optimized version of Bubble Sort
+{
+	int i, j;
+	bool swapped;
+	for (i = 0; i < n - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(&arr[j], &arr[j + 1]);
+				swapped = true;
+			}
+		}
+
+		// IF no two elements were swapped by inner loop, then break
+		if (swapped == false)
+			break;
+	}
+}
+
+void bubbleSort::printArray(int arr[], int size)// Function to print an array
+{
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+}
 void readin()
 {
 	ifstream thefile1("numbers.txt");//Textfile declaration
@@ -47,7 +101,7 @@ void readin()
 	_getch();//wait
 	//TODO: For making this into a full read in function, it should return the array so the sort can process the data
 }
-void writeout()
+void writeoutfile()
 {
 	ofstream thefile;
 	thefile.open("example.txt");//INPUT THE FILE DIR
@@ -58,16 +112,26 @@ void writeout()
 int main()
 {
 	int userchoice;
-	cout << "Would you like to 1.) read in, or 2.) print out?\n";
+	cout << "How would you like to sort these numbers?\n1.) Bubble.\n2.) Selection.\n3.) Insertion\n4.) Hash\n5.) Binary\n";
 	cin >> userchoice;
 	switch (userchoice)
 	{
-	case 1 :
-		readin();
+	case 1 ://Bubble
+		bubbleSort sort;
+		sort.bubbleSortDriver();
 		break;
-	case 2 :
-		writeout();
-	default:
+	case 2 ://Selection 
+		
+	case 3://Insertion
+		
+		break;
+	case 4://Hash
+		
+		break;
+	case 5://Binary
+		
+		break;
+	default://Error Catch Here!
 		break;
 	}
 	return 0;
